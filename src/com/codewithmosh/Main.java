9745a83b32a1d91b07bded022d91d3335a9b14d0
Juan Cuadros <juan.cuadros@pm.me>
2022-04-13 10:17:17 -0500

    Implement memento pattern.

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,65 +1,30 @@
 package com.codewithmosh;
 
 import com.codewithmosh.memento.Editor;
+import com.codewithmosh.memento.History;
 
 public class Main {
 
     public static void main(String[] args) {
-        // basic scaffolding
-        User user = new User("Mosh");
-        user.sayHello();
+        var editor = new Editor();
+        var history = new History();
 
-        /**
-         * Interfaces : contract that specifies the capabilities
-         * that a class should provide
-         */
-        System.out.println("--- Interfaces ---");
-        TaxCalculator calculator = getCalculator();
-        calculator.calculateTax();
+        editor.setContent("a");
+        history.push(editor.createState());
+        System.out.println(editor.getContent());
 
-        /**
-         * Encapsulation : bundling the data and methods
-         * that operate on the data within one unit (Class)
-         * and hiding the values (state of an object) inside
-         * the class
-         */
-        System.out.println("--- Encapsulation ---");
-        var account = new Account();
-        account.deposit(10);
-        System.out.println(account.getBalance());
+        editor.setContent("b");
+        history.push(editor.createState());
+        System.out.println(editor.getContent());
 
-        account.withdraw(5);
-        System.out.println(account.getBalance());
+        editor.setContent("c");
+        System.out.println(editor.getContent());
 
-        /**
-         * Abstraction : change the implementation details without
-         * affecting others classes in the application
-         */
-        System.out.println("--- Abstraction ---");
-        MailService mailService = new MailService();
-        mailService.sendEmail();
+        editor.restore(history.pop());
+        System.out.println(editor.getContent());
 
-        /**
-         * Inheritance : mechanism to reduce code across our classes
-         * allows the DRY principle (Don't Repeat Yourself)
-         */
-        System.out.println("--- Inheritance ---");
-        var textBox = new TextBox();
-        textBox.enable();
-
-        /**
-         * Polymorphism : ability of an object to take on many forms
-         * in this case UIControl takes TextBox and CheckBox forms
-         */
-        drawUIControl(new TextBox());
-        drawUIControl(new CheckBox());
+        editor.restore(history.pop());
+        System.out.println(editor.getContent());
     }
 
-    public static TaxCalculator getCalculator() {
-        return new TaxCalculator2021();
-    }
-
-    public static void drawUIControl(UIControl control) {
-        control.draw();
-    }
 }
