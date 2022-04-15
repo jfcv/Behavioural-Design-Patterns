14f21c5829d88ea56fd1ea3e41e6e3eaaced1679
Juan Cuadros <juan.cuadros@pm.me>
2022-04-15 09:40:17 -0500

    Implement iterator pattern.

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,5 +1,7 @@
 package com.codewithmosh;
 
+import com.codewithmosh.iterator.BrowseHistory;
+import com.codewithmosh.iterator.Iterator;
 import com.codewithmosh.memento.exercise.Document;
 import com.codewithmosh.memento.exercise.History;
 import com.codewithmosh.state.BrushTool;
@@ -11,22 +13,16 @@ import com.codewithmosh.state.exercise.*;
 
 public class Main {
     public static void main(String[] args) {
-        var directionService = new DirectionService();
+        var history = new BrowseHistory();
+        history.push("a");
+        history.push("b");
+        history.push("c");
 
-        directionService.setTravelMode(new Driving());
-        directionService.getEta();
-        directionService.getDirection();
-
-        directionService.setTravelMode(new Bicycling());
-        directionService.getEta();
-        directionService.getDirection();
-
-        directionService.setTravelMode(new Transit());
-        directionService.getEta();
-        directionService.getDirection();
-
-        directionService.setTravelMode(new Walking());
-        directionService.getEta();
-        directionService.getDirection();
+        Iterator iterator = history.createIterator();
+        while(iterator.hasNext()) {
+            var url = iterator.current();
+            System.out.println(url);
+            iterator.next();
+        }
     }
 }
